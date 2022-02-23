19405181181950c60ec408b1270fccd9f8043b83
Brian Aston <astonb.dev@gmail.com>
2022-02-22 17:04:19 -0800

    Working on axios mock

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,18 @@
 "use strict";
 
+const AxiosMockAdapter = require(
+  "axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
 const {
   shipProduct,
 } = require("./shipItApi");
 
 
 test("shipProduct", async function () {
+  axiosMock.onPost("http://localhost:3001/ship")
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",

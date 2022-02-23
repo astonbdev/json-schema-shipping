081c9d5ddb0e4ae6b633fe59e94a7e6bea0d09c8
Brian Aston <astonb.dev@gmail.com>
2022-02-22 16:04:16 -0800

    Finished implementing JSON validation

--- routes/shipments.js
+++ routes/shipments.js
@@ -5,6 +5,11 @@ const router = new express.Router();
 
 const { shipProduct } = require("../shipItApi");
 
+const { BadRequestError } = require("../expressError");
+
+const jsonschema = require("jsonschema");
+const shipmentSchema = require("../schemas/shipment.schema.json")
+
 /** POST /ship
  *
  * VShips an order coming from json body:
@@ -14,6 +19,12 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
+  const result = jsonschema.validate(req.body, shipmentSchema, {required: true});
+  if(!result.valid){
+    let errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
